84edb689b458157f618701e139289a8793ec4758
rpppT <tony642873@gmail.com>
2018-10-05 20:11:36 +0900

    π

---
+++ PI.py
@@ -0,0 +1,36 @@
+from matplotlib import pyplot as frame 
+from math import sin 
+from math import radians
+from time import sleep
+
+
+if __name__ == "__main__":
+    print "The Radius is fixed in 1"
+    print "the variable n is proption to calculate pi "
+    n =  input("N = ") 
+    r =  input("R =")
+   
+    x = 2*r*sin(radians(180.0 / float(n)))
+    pi = (n*x)/(2*r)
+    print "pi = %f" %(pi)
+    print "the circleference = %f" %(pi * 2 * r)
+    
+    sleep(5)
+    
+    n = 1 
+    r = 1 
+   
+    file = open('PI.txt', 'w')
+
+    while True:
+        x = 2 * r *sin(radians(180.0 / float(n)))
+        pi = (n*x)/(2*r)
+        file.write(str(pi)+"\n") 
+        n += 1 
+        print "try [{}] : {}".format(n , pi)
+    #    sleep(0.000001)
+
+    file.close()
+        
+   
+    
---
+++ READMe.md
@@ -0,0 +1,1 @@
+#Calculating π is to use the method which is increasing the number , to divide 180 / n, and to input sin. this function seeks sin result angle. 
